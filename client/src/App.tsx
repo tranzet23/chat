c2453405e88abbb34d7072915bda5c53d6ed8a0c
Yaroslav <yaroslav.studenikin@noveogroup.com>
2022-08-12 15:43:22 +0300

    create server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,10 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Messages from "./components/Messages/Messages";
 import Chat from "./components/Chat/Chat";
-import {useAppSelector} from "./hooks/redux";
 
 function App() {
-    const { isFetching } = useAppSelector(state => state.authReducer);
-
-    console.log(isFetching);
-
     return (
         <div className="App">
-
             <Header/>
             <div className="wrapper">
             <Messages/>
